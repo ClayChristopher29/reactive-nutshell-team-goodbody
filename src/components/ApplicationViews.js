a5042e3c54501e3e78433ad92c4542f288b0ac1f
MattRowee <29.matt@gmail.com>
2019-03-18 15:00:41 -0400

    Register is working!

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,17 +1,36 @@
 import { Route, Redirect } from "react-router-dom";
 import React, { Component } from "react";
 
+import Register from './authentication/register'
+
+import RegisterManager from './modules/registerManager'
+
 export default class ApplicationViews extends Component {
+
+  state={
+      // what goes in state in an application with multiple functionalities and components???
+  }
+
+  registerUser = userObject =>
+RegisterManager.postUser(userObject);
+
+  // componentDidMount() {
+
+  // }
 
   render() {
     return (
       <React.Fragment>
 
         <Route
-          exact path="/" render={props => {
-            return null
+          path="/"
+          render={props => {
+            return (
+              <Register
+              {...props}
+              registerUser={this.registerUser}/>
             // Remove null and return the component which will show news articles
-          }}
+            )}}
         />
 
         <Route
@@ -34,7 +53,7 @@ export default class ApplicationViews extends Component {
             // Remove null and return the component which will show the user's tasks
           }}
         />
-        
+
       </React.Fragment>
     );
   }
